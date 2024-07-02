03f2e61ba8c6fac2b4aa09d080cb5b53ba863f54
Ilya Maslyanyi <maslanyjila@gmail.com>
2024-07-02 18:45:25 +0300

    first commit

--- src/main/java/org/example/TryCatchP.java
+++ src/main/java/org/example/TryCatchP.java
@@ -1,9 +1,12 @@
 package org.example;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class TryCatchP extends JFrame {
+public class TryCatchP extends JFrame implements ActionListener {
     JTextField textField;
+    JButton button;
     TryCatchP () {
         setSize(500, 500);
         setTitle("Try Catch Practice");
@@ -17,12 +20,25 @@ public class TryCatchP extends JFrame {
     public void ui () {
         textField = new JTextField();
         textField.setBounds(50, 50, 200, 30);
-
+        textField.addActionListener(this);
         add(textField);
 
-        JButton button = new JButton("Click");
-        button.setBounds(50, 100, 100, 30);
+        button = new JButton("Click");
+        button.setBounds(50, 100, 75, 30);
+        button.addActionListener(this);
         add(button);
 
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == button) {
+            try{
+                textField.setText("");
+
+            }catch (){
+
+            }
+        }
     }
 }
