2b218e80da982b3feb7ace4213f1ddc5595ce220
Ilya Maslyanyi <maslanyjila@gmail.com>
2024-07-02 23:23:53 +0300

    first commit

--- src/main/java/org/example/TryCatchP.java
+++ src/main/java/org/example/TryCatchP.java
@@ -3,14 +3,19 @@ package org.example;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class TryCatchP extends JFrame implements ActionListener {
     JTextField textField;
-    JButton button;
+    JButton button, arrayCheck;
     JLabel label;
-    boolean isFail = false;
     int y = 200;
-    TryCatchP () {
+    List<Integer> numbers = new ArrayList<>();
+    List<Integer> limitedNumber;
+    int  maxSize = 5;
+    TryCatchP() {
         setSize(500, 500);
         setTitle("Try Catch Practice");
         setDefaultCloseOperation(EXIT_ON_CLOSE);
@@ -20,7 +25,7 @@ public class TryCatchP extends JFrame implements ActionListener {
 
     }
 
-    public void ui () {
+    public void ui() {
         textField = new JTextField();
         textField.setBounds(50, 50, 200, 30);
         textField.addActionListener(this);
@@ -31,23 +36,41 @@ public class TryCatchP extends JFrame implements ActionListener {
         button.addActionListener(this);
         add(button);
 
+        arrayCheck = new JButton("AddNumberInArray");
+        arrayCheck.setBounds(50, 150, 200, 30);
+        arrayCheck.addActionListener(this);
+        add(arrayCheck);
+        List<Integer> numbers = new ArrayList<>();
+
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
         if (e.getSource() == button) {
-            try{
+            try {
                 String text = textField.getText();
                 int number = Integer.parseInt(text);
-                label = new JLabel("You entered: " + textField.getText());
+                label = new JLabel("Your ID: " + textField.getText());
                 textField.setText("");
-                label.setBounds(50, y , 200, 40);
-                y+=40;
+                label.setBounds(50, y, 200, 40);
+                y += 40;
                 add(label);
                 revalidate();
                 repaint();
-            }catch (NumberFormatException ex){
+            } catch (NumberFormatException ex) {
                 System.out.println("You mustn`t have a litters");
+            }
+        }
+        if (e.getSource() == arrayCheck) {
+            try {
+                if (numbers.size() < maxSize) {
+                    numbers.add(numbers.size() + 1);
+                    System.out.println("Добавлен элемент. Текущий список: " + numbers);
+                }else {
+                    throw new IndexOutOfBoundsException();
+                }
+            } catch (IndexOutOfBoundsException ex) {
+                System.out.println("Limit size in list: 5");
             }
         }
     }
